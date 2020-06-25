e8b86688bb533be0fb8d490056222c3f7f0d6ea8
TobyGroves <Toby.tobtob@gmail.com>
2020-06-25 14:58:32 +0100

    started on the data buffer thing but stopped because its 30 degrees and im melting

---
+++ MoCapTest/Assets/MoCapBufferTest.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MoCapBufferTest : MonoBehaviour
+{
+    #region jsonHandeller_Structs
+    [System.Serializable]
+    struct mpuMoveInfo
+    {
+        public Vector3 accel_data;
+        public Vector3 gyro_data;
+        public float timeSinceLastPoll;
+    }
+    [System.Serializable]
+    struct DataStruct
+    {
+        public mpuMoveInfo[] mpuMoveList;
+    }
+    #endregion
+
+    [SerializeField]
+    string url = "http://192.168.0.144:5000/";
+
+    Queue<mpuMoveInfo> movementBuffer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementBuffer = new Queue<mpuMoveInfo>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void getData()
+    {
+
+    }
+    IEnumerator getDataCoroutine()
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url+"getDataMaxfps"))
+        {
+
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log("Error: " + webRequest.error);
+            }
+            else
+            {
+                setData(JsonUtility.FromJson<DataStruct>(webRequest.downloadHandler.text));
+
+                //dataRecived = true;
+            }
+        }
+    }
+    void setData(DataStruct _dataIn)
+    {
+        for (int i = 0; i < _dataIn.mpuMoveList.Length; i++)
+        {
+            movementBuffer.Enqueue(_dataIn.mpuMoveList[i]);
+        }
+    }
+    void updatePosition()
+    {
+
+    }
+
+    void calibrate()
+    {
+
+    }
+
+    IEnumerator calibrateCoroutine()
+    {
+
+        yield return null;
+    }
+
+}
--- MoCapTest/Assets/MocapTest1.cs
+++ MoCapTest/Assets/MocapTest1.cs
@@ -124,54 +124,10 @@ public class MocapTest1 : MonoBehaviour
 
         ComplementaryGyroFilter(currentGyro, timeSinceLastPoll);
 
-        //targetObject.transform.eulerAngles = new Vector3(pitch, 0, 0);
-
         targetObject.transform.RotateAround(targetObject.transform.position, targetObject.transform.right, xRotNode);
         targetObject.transform.RotateAround(targetObject.transform.position, targetObject.transform.up, -zRotNode);
         targetObject.transform.RotateAround(targetObject.transform.position, targetObject.transform.forward, yRotNode);
 
-
-        //targetObject.transform.localEulerAngles += new Vector3(xRotNode, -yRotNode, zRotNode);
-
-        // x is right
-        // y is up
-        // z is forward
-        //Debug.DrawRay(targetObject.transform.position, targetObject.transform.right * 10f, Color.red);
-        //Debug.DrawRay(targetObject.transform.position, targetObject.transform.up * 10f, Color.green);
-        //Debug.DrawRay(targetObject.transform.position, targetObject.transform.forward * 10f, Color.blue);
-
-
-        /*Vector3 xRotVector = new Vector3 (targetObject.transform.right.x * xRotNode,
-            targetObject.transform.right.y * xRotNode, targetObject.transform.right.z * xRotNode);
-
-        Vector3 yRotVector = new Vector3(targetObject.transform.up.x * -zRotNode, 
-            targetObject.transform.up.y * -zRotNode, targetObject.transform.up.z * -zRotNode);
-
-        Vector3 zRotVector = new Vector3(targetObject.transform.forward.x * yRotNode, 
-            targetObject.transform.forward.y * yRotNode, targetObject.transform.forward.z * yRotNode);*/
-
-        //targetObjectRotation = (xRotVector + yRotVector + zRotVector);
-
-        //targetObject.transform.eulerAngles = targetObject.transform.eulerAngles + targetObjectRotation;
-        //Debug.Log(xRotVector); // seems ok 
-        //Debug.Log(targetObject.transform.localRotation.eulerAngles.x); // clamps at 270 WHYYYYY ?
-
-        /*targetObject.transform.eulerAngles = 
-            new Vector3((targetObject.transform.eulerAngles.x + targetObjectRotation.x)%360, 
-            (targetObject.transform.eulerAngles.y + targetObjectRotation.y) % 360, 
-            (targetObject.transform.eulerAngles.z + targetObjectRotation.z) % 360);//% 360f;*/
-
-        //targetObject.transform.eulerAngles += targetObjectRotation;
-        //targetObject.transform.Rotate(targetObjectRotation);
-        //targetObject.transform.localRotation = Quaternion.Euler((targetObject.transform.localRotation.eulerAngles +targetObjectRotation));
-        //+= targetObjectRotation;
-
-
-
-
-        //targetObject.transform.localEulerAngles = new Vector3(xRotNode,-zRotNode, yRotNode);
-        // current accel divided by 1g raw to convert into g's then times by 9.8067 to get it in meters per 
-        // second squared
         velocity += ((((currentAccel*100) / 16384) * 9.8067f) * timeSinceLastPoll) / 2;
 
 
@@ -185,9 +141,6 @@ public class MocapTest1 : MonoBehaviour
         //targetObject.transform.localPosition += velocity * Time.deltaTime;
         if (Input.GetKeyDown("space"))
         {
-            roll = 0;
-            pitch = 0;
-            yaw = 0;
             targetObject.transform.localEulerAngles = new Vector3(0, 0, 0);
 
         }
@@ -200,16 +153,9 @@ public class MocapTest1 : MonoBehaviour
     float M_PI = 3.14159265359f;
     void ComplementaryGyroFilter(Vector3 _gyroData, float _dt)
     {
-
-        pitch += ((float)_gyroData.x / 131) * _dt;
-        roll += ((float)_gyroData.y / 131) * _dt;
-        yaw += ((float)_gyroData.z / 131) * _dt;
-
-
-        xRotNode = ((float)_gyroData.x / 131) * _dt; // clamps at 270 ? 
+        xRotNode = ((float)_gyroData.x / 131) * _dt;
         yRotNode = ((float)_gyroData.y / 131) * _dt;
         zRotNode = ((float)_gyroData.z / 131) * _dt;
-
     }
 
     IEnumerator calibrate()
